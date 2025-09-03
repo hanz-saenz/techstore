c0b3163a44be706fefd935712f1f418189e24235
FinMaq <hsaenz@finmaq.com.co>
2025-09-02 20:56:19 -0500

    Actualizar estilos y scripts para el carrito y productos; agregar lógica para mostrar productos y mensajes

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,5 +1,15 @@
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
             
+function mostrarMensaje(mensaje) {
+    let mensajeElemento = document.getElementById('mensaje');
+    mensajeElemento.innerText = mensaje;
+    mensajeElemento.style.display = 'block';
+    // tiempo de visualizacion del mensaje
+    setTimeout(() => {
+        mensajeElemento.style.display = 'none';
+    }, 3000);
+}
+
 function agregarAlCarrito(id) {
     let producto = productos.find(p => p.id === id);
     if (producto.stock > 0) {
@@ -7,6 +17,7 @@ function agregarAlCarrito(id) {
         console.log(`Producto agregado al carrito: ${producto.nombre}`);
         let itemAgregar = { ...producto, cantidad: 1};
         let existente = carrito.find(item => item.id === id);
+        mostrarMensaje(`Producto agregado al carrito: ${producto.nombre}`);
         if(existente) {
             existente.cantidad++;
         } else {
@@ -19,3 +30,4 @@ function agregarAlCarrito(id) {
         console.log(`Producto agotado: ${producto.nombre}`);
     }
 }
+
--- assets/js/productos.js
+++ assets/js/productos.js
@@ -118,3 +118,52 @@ const productos = [
     }
 
 ]
+
+
+//cargar nuestros productos
+
+let divProductos = document.querySelector(".productos__grid");
+
+function htmlProducto(producto) {
+    let descripcionCorta = producto.descripcion.length > 80
+                ? producto.descripcion.substring(0, 77) + "..."
+                : producto.descripcion;
+    return divProductos.innerHTML += `
+            <div class="producto">
+                <img src="${producto.imagen}" alt="${producto.nombre}" class="producto__imagen">
+                <h3 class="producto__nombre">${producto.nombre}</h3>
+                <p class="producto__precio">Precio: $${producto.precio}</p>
+                <p class="producto__stock">Stock: ${producto.stock}</p>
+                <p class="producto__descripcion">${descripcionCorta}</p>
+                <button class="producto__boton" ${producto.stock === 0 ? 'disabled' : ''} onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
+            </div>
+            `
+}
+
+function mostrarProductos(categoria="todos") {
+    divProductos.innerHTML = "";
+    productos
+        .filter(producto => categoria ===  "todos" || producto.categoria === categoria)
+        .forEach(producto => {
+            htmlProducto(producto);
+        })
+}
+
+mostrarProductos();
+
+function mostrarProductosSeleccionados() {
+    let categoriasSeleccionados = document.getElementById("filtro-producto");
+
+    //obtener la lista de categorias
+    let categorias = Array.from(categoriasSeleccionados.selectedOptions).map(option => option.value);
+    console.log(categorias);
+    divProductos.innerHTML = "";
+    productos
+        .filter(producto => 
+            categoriasSeleccionados.value === "todos" || categorias.includes(producto.categoria))
+        .forEach(producto => {
+            htmlProducto(producto);
+        })
+}
+
+mostrarProductosSeleccionados();
